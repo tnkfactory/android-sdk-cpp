6c9885c30870ea7799ea991fa87371e9cb8dc551
tnkfactory <58322808+tnkfactory@users.noreply.github.com>
2022-07-22 09:50:12 +0900

    tnk-sdk-cpp sample upload

---
+++ sample_app/app/src/main/java/com/tnkfactory/tnk_sample_cpp/MainActivity.java
@@ -0,0 +1,66 @@
+package com.tnkfactory.tnk_sample_cpp;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.tnkfactory.ad.TemplateLayoutUtils;
+import com.tnkfactory.ad.TnkSession;
+import com.tnkfactory.ad.cpp.CashpoppopPlus;
+import com.tnkfactory.ad.cpp.CppAdViewListener;
+import com.tnkfactory.ad.cpp.CppConfig;
+import com.tnkfactory.ad.cpp.CppMainViewId;
+import com.tnkfactory.tnk_sample_cpp.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    final static String TAG = "MainActivity";
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // 유저 고유 식별값 설정
+        TnkSession.setUserName(MainActivity.this, "user_uuid");
+
+        // Analytics Report 사용을 위한 앱 시작 로깅
+        TnkSession.applicationStarted(this);
+
+        // COPPA 설정 (true - ON / false - OFF)
+        TnkSession.setCOPPA(MainActivity.this, false);
+
+        CppConfig config = new CppConfig.Builder()
+                .tnkLayout(TemplateLayoutUtils.getBlueTabStyle_01())
+                .listener(adViewListener)
+                .build();
+
+        CashpoppopPlus cpp = new CashpoppopPlus(MainActivity.this, config);
+        cpp.showAdView(this);
+
+    }
+
+    CppAdViewListener adViewListener = new CppAdViewListener() {
+        @Override
+        public void onClickMenu(CppMainViewId id) {
+            super.onClickMenu(id);
+            switch (id) {
+                case PPI:
+                    Log.d(TAG, "onClick 보상형");
+                    break;
+                case CPS:
+                    Log.d(TAG, "onClick 구매형");
+                    break;
+            }
+        }
+
+        @Override
+        public void onDismiss() {
+            super.onDismiss();
+        }
+    };
+}
